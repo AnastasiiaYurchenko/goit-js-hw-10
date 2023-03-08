83d53987cb44168d287c4531fdebceeb761396fb
AnastasiiaYurchenko <nastya.brok@ukr.net>
2023-03-09 01:13:11 +0200

    add Notiflix, debounce

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,4 +1,11 @@
+import Notiflix from 'notiflix';
 
 export function fetchCountries(name) {
-    return fetch(`https://restcountries.com/v3.1/name/${name}`).then((response) => response.json());
+    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name.official,capital,population,flags.svg,languages`)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error (Notiflix.Notify.failure('Oops, there is no country with that name'))
+            }
+            response.json()
+        } );
 };
--- src/index.js
+++ src/index.js
@@ -1,25 +1,33 @@
 import './css/styles.css';
-import {fetchCountries} from "./fetchCountries"
+import { fetchCountries } from "./fetchCountries";
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 
-const DEBOUNCE_DELAY = 300;
+const DEBOUNCE_DELAY = 3000;
 
 const searchCountry= document.querySelector("#search-box");
 const countryList = document.querySelector(".country-list");
 const countryInfo = document.querySelector(".country-info");
-console.log(countryInfo);
+// console.log(countryInfo);
 
-searchCountry.addEventListener("input", onSearchCountryInput);
+searchCountry.addEventListener("input", debounce(onSearchCountryInput, DEBOUNCE_DELAY));
 
 function onSearchCountryInput(e) {
-    e.preventDefault();
+    // e.preventDefault();
     console.log("onSearchCountryInput");
 
-    const input = e.currentTarget;
+    const input = e.target;
     console.log(input);
     const countryName = input.value.trim();
     console.log(countryName);
 
     fetchCountries(countryName)
-        // .then((response) => response.json())
-        // .then((data) => console.log(data))
+        .then((data) => {
+            if (data.length > 10) {
+                // console.log ("забагато країн")
+                Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+            }
+            console.log(data)
+        } )
+        .catch((error) => {console.log(error)})
 }
